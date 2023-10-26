088fb98101e3dd6337ba8ac40636712f1490b4fc
Thrustgamers <oke.leukk@gmail.com>
2023-10-26 14:33:27 +0200

    Added React

--- server.go
+++ server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Static("/", "./web/dist/")
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		fmt.Print(c.IP())
-		return c.SendStatus(200)
+		return c.SendString("test")
+	})
+
+	app.Get("/user/checklogin/:id", func(c *fiber.Ctx) error {
+
+		return c.SendString("test")
 	})
 
 	app.Listen(":3000")
---
+++ web/src/app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Layers
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+
+export default App;
