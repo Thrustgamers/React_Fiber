57c3af8cb274953c619641aabf56f23587fe79a5
Thrustgamers <oke.leukk@gmail.com>
2023-11-14 22:40:11 +0100

    add(main) highlight added

--- main.go
+++ main.go
@@ -7,16 +7,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/highlight/highlight/sdk/highlight-go"
+	highlightFiber "github.com/highlight/highlight/sdk/highlight-go/middleware/fiber"
 )
 
 var app *fiber.App
 
 func main() {
 	database.ConnectToDb()
+
+	highlight.SetProjectID("")
+	highlight.Start(highlight.WithServiceName("go_webshop"), highlight.WithServiceVersion("git-sha"))
+
 	app = fiber.New()
 	app.Use(cors.New())
-	routes.SetupRoutes(app)
 	app.Get("/metrics", monitor.New())
 	app.Static("/", "./web/dist/")
+	app.Use(highlightFiber.Middleware())
+
+	routes.SetupRoutes(app)
+
 	app.Listen(":80")
 }
