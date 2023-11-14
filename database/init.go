37b99bec00c7e1f4ad797c43c77b692a6f556fbf
Thrustgamers <oke.leukk@gmail.com>
2023-11-14 22:41:03 +0100

    test(init) New init testing

--- database/init.go
+++ database/init.go
@@ -1,10 +1,17 @@
 package database
 
-import "api/models"
+import (
+	"api/models"
+	"log"
+)
 
 func Init() {
 
-	item := &models.Items{name: "test", serial: 512135435}
+	item := models.Items{Name: "test", Serial: "512135435"}
 
-	Database.Db.Create(&item)
+	db := Database.Db.Where(&models.Items{Serial: "512135435"}).FirstOrCreate(&item)
+
+	if db.Error != nil {
+		log.Fatal(db.Error)
+	}
 }
